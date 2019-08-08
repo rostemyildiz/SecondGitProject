507f4f40c5403e37974a0c8e8744147c002795dc
Medeni Rostem <mrostem04@gmail.com>
2019-08-08 11:31:10 -0500

    first commit

---
+++ src/test/java/com/vytrack/tests/TestFirst.java
@@ -0,0 +1,9 @@
+package com.vytrack.tests;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class TestFirst {
+    public static void main(String[] args) {
+        System.out.println("heloo kubanic artislik yapma");
+    }
+}
